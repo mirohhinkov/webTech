197bf494a168a039fe402a688cdce1184d564572
mirohhinkov <67483418+mirohhinkov@users.noreply.github.com>
2022-01-12 12:58:27 +0000

    Update dbController.js

--- Controllers/dbController.js
+++ Controllers/dbController.js
@@ -2,13 +2,13 @@ const mysql = require("mysql");
 // const catchAsync = require('./catchAsynccatchAsync');
 
 exports.request = (req, res) => {
-  if (process.env.NODE_ENV == "development") console.log(`query - ${req.sql}`);
+  // if (process.env.NODE_ENV == "development") console.log(`query - ${req.sql}`);
   return new Promise((resolve, reject) => {
     const dB = mysql.createConnection(req.dataBase);
     dB.connect((err) => {
       if (err) reject(err);
-      if (process.env.NODE_ENV == "development") console.log("Connected!");
-      dB.query(req.sql, (err, result) => {
+      // if (process.env.NODE_ENV == "development") console.log("Connected!");
+      dB.query(req.sql, req.sqlArgs, (err, result) => {
         if (err) reject(err);
         resolve(result);
       });
